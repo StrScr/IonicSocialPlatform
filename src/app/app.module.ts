de0bd999371e08d57acddc9e5e53c28d3c88c329
StrScr <andres.mejia@unitec.edu>
2018-03-10 16:57:58 -0600

    Firebase config and AnfularFire Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule} from 'angularfire2/auth';
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyB8SofxpXMiBjfCIE-jSWs3F2UOCubQ4JA",
+  authDomain: "ionic-social-platform.firebaseapp.com",
+  databaseURL: "https://ionic-social-platform.firebaseio.com",
+  projectId: "ionic-social-platform",
+  storageBucket: "ionic-social-platform.appspot.com",
+  messagingSenderId: "658928932316"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
