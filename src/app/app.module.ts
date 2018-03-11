a4a068d37aca20607aaf4f38e4523dcd3edeb028
StrScr <andres.mejia@unitec.edu>
2018-03-11 14:14:27 -0600

    Use AF2 database module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomePage } from '../pages/home/home';
 
 // Import the AF2 Module
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuthModule} from 'angularfire2/auth';
 // AF2 Settings
 export const firebaseConfig = {
@@ -41,6 +41,7 @@ export const firebaseConfig = {
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
