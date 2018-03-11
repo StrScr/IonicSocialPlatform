24f82e648fc3244558f35085127427007e1dac8a
StrScr <andres.mejia@unitec.edu>
2018-03-11 14:14:53 -0600

    Google auth

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  currentUser: any;
+
+  constructor(
+    public navCtrl: NavController,
+    afDB: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) {
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        this.currentUser = null;
+        return;
+      }
+      this.currentUser = {handle: null, uid: user.uid, isAnon: user.isAnonymous};
+    });
+  }
+
+  signInWithGoogle() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(res => console.log(res));
 
   }
 
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
 }
